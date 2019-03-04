78e6a3bdcbda88db5b52ac8b351c51aee093d3d1
Maxwell Reynolds <maxreynolds55@gmail.com>
2019-03-04 15:28:16 -0700

    Max's Work initial commit

--- angular-workspace/src/app/app.module.ts
+++ angular-workspace/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    TimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-workspace/src/app/components/user/user.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  address:Address;
+  beers:string[];
+  constructor() { }
+
+  ngOnInit() {
+      this.name='No Name';
+      this.age= 0;
+      this.email='nn@gmail.com'
+      this.beers=['Stout', 'Porter', 'Amber'];
+      this.address = {
+          street: '0 main st',
+          city: 'Boulder',
+          state:'CO'
+      }
+
+  }
+  onClick(){
+      this.name="Max Reynolds";
+      this.email='maxreynolds55@gmail.com'
+      this.address.street='100 Pearl Street'
+      this.age=23;
+      this.beers.push('Other')
+  }
+
+  addBeer(beer){
+      this.beers.unshift(beer)
+      return false;
+  }
+
+  deleteBeer(beer){
+      for(let i = 0; i <this.beers.length; i++){
+          if(this.beers[i] == beer){
+              this.beers.splice(i, 1)
+          }
+      }
+  }
+
+
+}
+
+interface Address{
+    street:string
+    city:string
+    state:string
+}
