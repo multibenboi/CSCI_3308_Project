dddb128c6a6681f6d89ade97eb8cce68cbbed70c
Maxwell Reynolds <maxreynolds55@gmail.com>
2019-03-08 15:52:00 -0700

    Added a basic timer

--- angular-workspace/src/app/components/timer/timer.component.ts
+++ angular-workspace/src/app/components/timer/timer.component.ts
@@ -5,11 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent {
+    timeLeft: number = 20;
+    interval;
+    paused: boolean = true; //to only allow start if not in progress
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    startTimerIfPaused(){
+        if (this.paused){
+            this.startTimer();
+        }
+    }
+    startTimer(){
+        this.paused = false
+        this.interval = setInterval(() =>{
+            if(this.timeLeft>0){
+                this.timeLeft--;
+            } else{
+                this.pauseTimer();
+                //this.changeTime(30);
+            }
+        },1000)
+    }
+
+    changeTime(t){
+        this.timeLeft=t;
+    }
+
+    pauseTimer(){
+        this.paused = true;
+        clearInterval(this.interval);
+    }
 
 }
