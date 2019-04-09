fd1f2a410d7e4d1d037fbc742c7a821f1dc0c156
tylernevell <tylernevell@gmail.com>
2019-04-09 15:43:15 -0700

    created a primitive nav bar. Added routes to each component we have so far.

--- angular-workspace/src/app/app.module.ts
+++ angular-workspace/src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import {Route, RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { TimerComponent } from './components/timer/timer.component';
 import { WebframeComponent } from './webframe/webframe.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'user', component: UserComponent },
+  { path: 'timer', component: TimerComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     TimerComponent,
-    WebframeComponent
+    WebframeComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
     AppRoutingModule,
     FormsModule
@@ -23,4 +35,5 @@ import { WebframeComponent } from './webframe/webframe.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
