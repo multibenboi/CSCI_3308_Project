f6562095169049bc70234961e99e8a6f8511ca96
tylernevell <tylernevell@gmail.com>
2019-04-09 12:06:48 -0700

    add webframe component

---
+++ angular-workspace/src/app/webframe/webframe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webframe',
+  templateUrl: './webframe.component.html',
+  styleUrls: ['./webframe.component.css']
+})
+export class WebframeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
