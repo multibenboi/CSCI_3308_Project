3a5be452e403061aa789d17aab7106283afee5f9
tylernevell <tylernevell@gmail.com>
2019-04-09 14:33:59 -0700

    border adjustments

--- angular-workspace/src/app/app.module.ts
+++ angular-workspace/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
